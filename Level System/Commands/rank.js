b225a542e8acb28c2815bd8acafb808947e8cee6
Jakub Górzny <jakubgorzny@icloud.com>
2022-09-21 19:42:31 +0200

    Update rank.js

--- Level System/Commands/rank.js
+++ Level System/Commands/rank.js
@@ -10,19 +10,25 @@ module.exports = {
          option.setName('member').setDescription('Target @member')
       ),
    async execute(interaction) {
-      const member = interaction.options.getMember('member');
+      const member =
+         interaction.options.getMember('member') || interaction.member;
 
-      const guildId = interaction.guildId;
-      console.log(guildId, member);
-      interaction.reply('xd');
-      // const userId = member
+      const guildId = member.guild.id;
+      const userId = member.user.id;
 
-      // let user;
+      let user;
 
-      // try {
-      //    user = await User.findOne({ guildId, userId });
-      // } catch (err) {
-      //    console.log(err);
-      // }
+      try {
+         user = await User.findOne({ guildId, userId });
+
+         let { xp, level } = user;
+
+         interaction.reply({
+            content: `<@${userId}>, your current rank is ${level} with ${xp}xp.`,
+         });
+      } catch (err) {
+         console.log(err);
+         interaction.reply({ content: 'Something went wrong, sorry :(' });
+      }
    },
 };
